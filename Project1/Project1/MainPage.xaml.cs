7d0fc17f3d66548d442b1e32a9732038a8ed67ca
AndersonSilvaIt <42393231+AndersonSilvaIt@users.noreply.github.com>
2025-01-10 11:28:52 -0300

    Start application

---
+++ Project1/Project1/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace Project1
+{
+    public partial class MainPage: ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            CounterBtn.Text = $"Contador atual {count}";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}

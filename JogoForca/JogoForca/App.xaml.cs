d9189d87eabd02c28d43377f19f04f02f74b85cd
AndersonSilvaIt <42393231+AndersonSilvaIt@users.noreply.github.com>
2025-01-13 19:02:40 -0300

    Jogo da Forca

---
+++ JogoForca/JogoForca/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace JogoForca
+{
+    public partial class App: Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ JogoForca/JogoForca/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace JogoForca
+{
+    public partial class AppShell: Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}

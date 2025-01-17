8d047b7aebcdd54f79381cc8eda50494c94b7464
anderson.msilva <anderson.msilva@terceiroslinx.com.br>
2025-01-17 16:00:36 -0300

    DataTemplate with CollectionView

---
+++ MauiCollectionView/MauiCollectionView/Selectors/ProdutoDataTemplateSelector.cs
@@ -0,0 +1,23 @@
+﻿using MauiCollectionView.MVVM.Models;
+
+namespace MauiCollectionView.Selectors
+{
+    public class ProdutoDataTemplateSelector : DataTemplateSelector
+    {
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var produto = item as Produto;
+
+            if (!produto.EmOferta)
+            {
+                Application.Current.Resources.TryGetValue("ProdutosStyle", out var produtosStyle);
+                return produtosStyle as DataTemplate;
+            }
+            else
+            {
+                Application.Current.Resources.TryGetValue("EmOfertaStyle", out var emOfertaStyle);
+                return emOfertaStyle as DataTemplate;
+            }
+        }
+    }
+}

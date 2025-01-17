4e8b2f30f52e95b0a6395754de0e2530e317c367
anderson.msilva <anderson.msilva@terceiroslinx.com.br>
2025-01-17 13:17:58 -0300

    Tarefas App

---
+++ MauiTarefas/MauiTarefas/MauiProgram.cs
@@ -0,0 +1,33 @@
+﻿using MauiTarefas.Pages;
+using MauiTarefas.ViewModels;
+using Microsoft.Extensions.Logging;
+
+namespace MauiTarefas
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            builder.Services.AddSingleton<MainPage>();
+            builder.Services.AddSingleton<TarefasPage>();
+            builder.Services.AddSingleton<DetalhesPage>();
+            builder.Services.AddSingleton<TarefasPageViewModel>();
+            builder.Services.AddSingleton<DetalhesPageViewModel>();
+          
+            return builder.Build();
+        }
+    }
+}

386a94808a5df873d1714b53ff0e24fd07baf262
anderson.msilva <anderson.msilva@terceiroslinx.com.br>
2025-01-17 13:32:27 -0300

    Estilos

---
+++ MauiEstilos1/MauiEstilos1/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiEstilos1
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiEstilos1/MauiEstilos1/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiEstilos1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
